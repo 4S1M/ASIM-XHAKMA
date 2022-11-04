e31e817d19db7afb7a2d9d98bcc11be3fc3088f1
ASIM XHAKMA <117442412+4S1M@users.noreply.github.com>
2022-11-04 19:41:06 +0600

    Add files via upload

---
+++ script.py
@@ -0,0 +1,17 @@
+import os
+os.system("clear")
+
+print(" \033[1;31m==>FREE SCRIPT COLLECT MY GITHUB REPOSITORIES ")
+print(" \033[1;32mFILE NAME =>[ FREE-SCRIPT ] ")
+ 
+print(" ==>HOWDY\033[1;31m  ITS\033[1;32m ASIM\033[1;33m  SAGOR ")
+print(" [1] FOLLOW\033[1;32m MY\033[1;34m  GITHUB\033[1;33m UNLOCK\033[1;37m SCRIPT ")
+print(" [2] JOIN\033[1;38m GROUP\033[1;39m GET\033[1;34m FREE\033[1;36m SCRIPT ")
+
+
+ASIM = input ( 'choose>===>> ' )
+if ASIM in ['1']:
+	os.system('xdg-open https://github.com/Toxic-ASIM')
+if ASIM in ['2']:
+		os.system('xdg-open https://facebook.com/groups/1215404685982268/')
+		
